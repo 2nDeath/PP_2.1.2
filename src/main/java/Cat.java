f37d6ccb415c8290ee3656b47a898498e12c2f89
Stepan Pechenkin <stepanvill2002@yandex.ru>
2022-12-16 14:46:40 +0300

    first version

---
+++ src/main/java/Cat.java
@@ -0,0 +1,21 @@
+public class Cat {
+
+    public Cat() {
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
